71c6b2dd89e905356702fbb1dd650d7c8d5130c5
ZioFester73 <>
2021-09-19 16:54:34 +0000

    Add stringTests

---
+++ tests/stringTests.py
@@ -0,0 +1,19 @@
+import unittest
+
+class stringFixture(unittest.TestCase):
+
+    def setUp(self):
+      pass
+
+    def tearDown(self):
+      pass
+
+    def testEmptySubstringByArrayIndexRaiseError(self):
+        with self.assertRaises(IndexError):
+            y = ''[0]
+
+    def testEmptySubstringByIndexesReturnEmpty(self):
+        self.assertEqual(''[0:],'')
+
+if __name__ == '__main__':
+    unittest.main()
