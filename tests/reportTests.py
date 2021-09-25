065e7b2182b1aebfff4e0876710605a056c1fd91
ZioFester73 <61948450+ZioFester73@users.noreply.github.com>
2021-09-25 06:20:46 +0000

    Before proceding need manage a testing db
    Added a skipped test that will have to pass in the future

--- tests/reportTests.py
+++ tests/reportTests.py
@@ -8,3 +8,17 @@ class reportFixtures(unittest.TestCase):
     outputReport = reportExpert.getReport()
     
     self.assertEqual(outputReport, 'column1|column2|column3', 'Show only column header for no value')
+
+  @unittest.skip("Need before a db")
+  def testARowFromDb(self):
+
+    dbContains('data1|data2|data3')
+
+    outputReport = reportExpert.getReport()
+
+    expected = ['column1|column2|column3', 'data1|data2|data3']
+    self.assertEqual(outputReport, expected, 'One line from db')
+
+
+def dbContains(aRow):
+  pass
